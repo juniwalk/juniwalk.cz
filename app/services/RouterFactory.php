bfb0d651a891af6cb0e2e1712bab624a91e0494f
Martin Procházka <juniwalk@outlook.cz>
2015-11-07 23:06:03 +0100

    RouterFactory: Moved to services

---
+++ app/services/RouterFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   www.juniwalk.cz
+ * @link      https://github.com/juniwalk/www.juniwalk.cz
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace App\Services;
+
+use Nette\Application\Routers\Route;
+use Nette\Application\Routers\RouteList;
+
+final class RouterFactory
+{
+    /**
+     * Create new list of routes.
+     * @return RouteList
+     */
+	public static function createRouter()
+	{
+        // Make this site HTTPS only
+        Route::$defaultFlags |= Route::SECURED;
+
+        // Create new Router instance
+        $router = new RouteList;
+        $router[] = new Route('<presenter>/<action>[/<id>]', 'Home:default');
+
+        return $router;
+	}
+}
