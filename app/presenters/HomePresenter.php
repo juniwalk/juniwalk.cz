aae60ad10663b8ac9bc4c6c357feb5d7cca24781
Martin Procházka <juniwalk@outlook.cz>
2015-11-07 23:11:16 +0100

    Rename Homepage to Home
    + Add base presenter
    + Add doc blocks

---
+++ app/presenters/HomePresenter.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   www.juniwalk.cz
+ * @link      https://github.com/juniwalk/www.juniwalk.cz
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace App\Presenters;
+
+class HomePresenter extends BasePresenter
+{
+}
