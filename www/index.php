8376607e9eab87631e160f4ed166d868eaff8487
Martin Procházka <juniwalk@outlook.cz>
2015-11-07 23:09:22 +0100

    Index: Clean up & add docblock

--- www/index.php
+++ www/index.php
@@ -1,4 +1,12 @@
 <?php
 
-$container = require __DIR__ . '/../app/bootstrap.php';
-$container->getByType('Nette\Application\Application')->run();
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   www.juniwalk.cz
+ * @link      https://github.com/juniwalk/www.juniwalk.cz
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+$di = include __DIR__.'/../app/bootstrap.php';
+$di->getByType('Nette\Application\Application')->run();
