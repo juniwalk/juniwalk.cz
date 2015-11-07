1ea9aa03564c69f7fec5f8a89ba690a52c627780
Martin Procházka <juniwalk@outlook.cz>
2015-11-07 23:11:25 +0100

    Added BasePresenter

---
+++ app/presenters/BasePresenter.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @author    Martin Procházka <juniwalk@outlook.cz>
+ * @package   www.juniwalk.cz
+ * @link      https://github.com/juniwalk/www.juniwalk.cz
+ * @copyright Martin Procházka (c) 2015
+ * @license   MIT License
+ */
+
+namespace App\Presenters;
+
+use Nette\Http\IResponse;
+use Nette\Utils\Strings;
+
+abstract class BasePresenter extends \Nette\Application\UI\Presenter
+{
+}
